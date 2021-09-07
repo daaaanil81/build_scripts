db1020b8a188ada24db24c70890848e249ae5992
Daniil Petrov <external.Daniil.Petrov@de.bosch.com>
2021-09-07 17:46:00 +0300

    Add Images and License_flag
    Signed-off-by: Daniil Petrov <external.Daniil.Petrov@de.bosch.com>

--- build.sh
+++ build.sh
@@ -13,11 +13,15 @@ bitbake-layers add-layer ../metas/meta-sunxi/
 bitbake-layers add-layer ../metas/meta-go-binary
 bitbake-layers add-layer ../metas/meta-camera/
 
-echo 'SSTATE_MIRRORS = "\
-     file://.* http://sstate.yoctoproject.org/dev/PATH;downloadfilename=PATH \n \
-     file://.* http://sstate.yoctoproject.org/3.0.2/PATH;downloadfilename=PATH \n \
-     file://.* http://sstate.yoctoproject.org/3.1/PATH;downloadfilename=PATH \n \
-     "' >> conf/local.conf
+#echo 'SSTATE_MIRRORS = "\
+#     file://.* http://sstate.yoctoproject.org/dev/PATH;downloadfilename=PATH \n \
+#     file://.* http://sstate.yoctoproject.org/3.0.2/PATH;downloadfilename=PATH \n \
+#     file://.* http://sstate.yoctoproject.org/3.1/PATH;downloadfilename=PATH \n \
+#     "' >> conf/local.conf
 
-MACHINE=qemux86-64 bitbake core-image-sato
+echo 'IMAGE_INSTALL_append = " vlc python3 python3-cmd2 go camera"' >> conf/local.conf
+
+echo 'LICENSE_FLAGS_WHITELIST="commercial"' >> conf/local.conf
+
+MACHINE=qemux86-64 bitbake core-image-minimal
 #MACHINE=orange-pi-one bitbake core-image-minimal
