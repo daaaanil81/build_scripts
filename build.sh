8a55e0cfc8096262f0c4d6df83fa6af0bf3647f7
Daniil Petrov <external.Daniil.Petrov@de.bosch.com>
2021-09-12 10:05:06 +0300

    Update metas for bitbake-layers
    Signed-off-by: Daniil Petrov <external.Daniil.Petrov@de.bosch.com>

--- build.sh
+++ build.sh
@@ -4,20 +4,10 @@ source poky/oe-init-build-env
 
 bitbake-layers add-layer ../metas/meta-openembedded/meta-oe
 bitbake-layers add-layer ../metas/meta-openembedded/meta-python/
-bitbake-layers add-layer ../metas/meta-openembedded/meta-perl/
 bitbake-layers add-layer ../metas/meta-openembedded/meta-networking/
-bitbake-layers add-layer ../metas/meta-openembedded/meta-webserver/
-bitbake-layers add-layer ../metas/meta-openembedded/meta-filesystems/
 bitbake-layers add-layer ../metas/meta-openembedded/meta-multimedia/
 bitbake-layers add-layer ../metas/meta-sunxi/
-bitbake-layers add-layer ../metas/meta-go-binary
 bitbake-layers add-layer ../metas/meta-camera/
-
-#echo 'SSTATE_MIRRORS = "\
-#     file://.* http://sstate.yoctoproject.org/dev/PATH;downloadfilename=PATH \n \
-#     file://.* http://sstate.yoctoproject.org/3.0.2/PATH;downloadfilename=PATH \n \
-#     file://.* http://sstate.yoctoproject.org/3.1/PATH;downloadfilename=PATH \n \
-#     "' >> conf/local.conf
 
 echo 'IMAGE_INSTALL_append = " vlc python3 python3-cmd2 go camera"' >> conf/local.conf
 
